6895ce22369449c9e89fe20b2e2343b913442261
iqbal <iqbal017376@gmail.com>
2023-03-14 09:25:42 +0800

    crud laravel

--- resources/views/Document/index.blade.php
+++ resources/views/Document/index.blade.php
@@ -9,34 +9,37 @@
 </head>
 
 <body>
-    <h1>My name is {{ $name }}</h1>
-    <p>I work at {{ $workplace }}</p>
+    <div align="center">
+        <h1>Senarai Dokument</h1>
+        {{-- <h1>My name is {{ $name }}</h1>
+        <p>I work at {{ $workplace }}</p> --}}
 
-    <table border="1">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Tarikh Di Terbitkan</th>
-                <th>Nama Penulis</th>
-                <th>Disahkan Oleh</th>
-                <th>Tarikh Disahkan</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($documents as $doc)
+        <table border="1">
+            <thead>
                 <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $doc->nama }}</td>
-                    <td>{{ $doc->tarikh_diterbitkan }}</td>
-                    <td>{{ $doc->nama_penulis }}</td>
-                    <td>{{ $doc->disahkan_oleh }}</td>
-                    <td>{{ $doc->tarikh_disahkan }}</td>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Tarikh Di Terbitkan</th>
+                    <th>Nama Penulis</th>
+                    <th>Disahkan Oleh</th>
+                    <th>Tarikh Disahkan</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $documents->links() }}
+            </thead>
+            <tbody>
+                @foreach ($documents as $doc)
+                    <tr>
+                        <td>{{ $documents->firstItem() + $loop->index }}</td>
+                        <td>{{ $doc->nama }}</td>
+                        <td>{{ $doc->tarikh_diterbitkan }}</td>
+                        <td>{{ $doc->nama_penulis }}</td>
+                        <td>{{ $doc->disahkan_oleh }}</td>
+                        <td>{{ $doc->tarikh_disahkan }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $documents->links() }}
+    </div>
 </body>
 
 </html>
